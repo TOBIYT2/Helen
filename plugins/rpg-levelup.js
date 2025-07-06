2a5b2423dc1b82b3bd2929ff76bf3f09fa9c5128
TOBI-12 <157072393+TOBI-12@users.noreply.github.com>
2025-07-06 11:26:39 -0600

    Add files via upload

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -2,29 +2,48 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-let img = await (await fetch(`https://files.catbox.moe/gjgkk4.jpg`)).buffer()
-let name = conn.getName(m.sender)
-let user = global.db.data.users[m.sender]
-if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-let { min, xp, max } = xpRange(user.level, global.multiplier)
-let txt = `💙 *Nombre* ${name}\n\n`
-txt += `💙 *Nivel* ${user.level}\n`
-txt += `💙 *XP* ${user.exp - min} / ${xp}\n\n`
-txt += `💙 No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}
-let before = user.level * 1
-while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-if (before !== user.level) {
-let txt = `🔥 F E L I C I T A C I O N E S 🔥\n\n` 
-txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
-txt += `• ✨ Nivel anterior : ${before}\n`
-txt += `• 🌟 Nuevos niveles : ${user.level}\n`
-txt += `• 💫 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
-txt += `🎩 *Nota:* _Cuanto más a menudo interactúes con *𝐑𝐞𝐦𝐁𝐨𝐭 *, mayor será tu nivel_`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}}
+  console.log('🧪 Comando nivel ejecutado correctamente');
+
+  let img = await (await fetch('https://files.catbox.moe/gjgkk4.jpg')).buffer()
+  let name = conn.getName(m.sender)
+  let user = global.db.data.users[m.sender]
+
+  if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+    let { min, xp, max } = xpRange(user.level, global.multiplier)
+    let txt = `💛 *Nombre:* ${name}\n\n`
+    txt += `💛 *Nivel:* ${user.level}\n`
+    txt += `💛 *XP:* ${user.exp - min} / ${xp}\n\n`
+    txt += `💛 Te faltan *${max - user.exp}* XP ¡Sigue intentando! ✨`
+
+    await conn.sendMessage(m.chat, {
+      image: img,
+      caption: txt
+    }, { quoted: m })
+
+    return
+  }
+
+  let before = user.level
+  while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+
+  if (before !== user.level) {
+    let txt = `🍭 *¡FELICIDADES!* 🍭\n\n` 
+    txt += `🎉 *${before}* ➔ *${user.level}* [ ${user.role} ]\n`
+    txt += `🧢 Nivel anterior: ${before}\n`
+    txt += `🧢 Nuevo nivel: ${user.level}\n`
+    txt += `📅 Fecha: ${new Date().toLocaleString('id-ID')}\n\n`
+    txt += `📌 *Consejo:* _Cuanto más uses a *Hinata-Bot*, más subirás de nivel_`
+
+    await conn.sendMessage(m.chat, {
+      image: img,
+      caption: txt
+    }, { quoted: m })
+  }
+}
 
 handler.help = ['levelup']
 handler.tags = ['rpg']
-handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
-handler.register = true 
+handler.command = ['nivel', 'lvl', 'levelup', 'level']
+handler.register = true
+
 export default handler
