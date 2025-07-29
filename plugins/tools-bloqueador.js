a64c75037586d326a5d4ad02466a5b724b4684fa
TOBI-12 <157072393+TOBI-12@users.noreply.github.com>
2025-07-28 22:53:47 -0600

    Update tools-bloqueador.js

--- plugins/tools-bloqueador.js
+++ plugins/tools-bloqueador.js
@@ -1,10 +1,17 @@
 import fs from 'fs'
 
 let handler = async (m, { conn }) => {
-  const estado = JSON.parse(fs.readFileSync('./estado-bot.json'))
+  let estado
+  try {
+    estado = JSON.parse(fs.readFileSync('./estado-bot.json'))
+  } catch (e) {
+    estado = { activo: true }
+  }
+
   const botNumber = conn.user.jid.split('@')[0]
   const senderNumber = m.sender.split('@')[0]
 
+  // Si el bot está apagado y el que habla no es el propio bot, bloquear todo
   if (!estado.activo && botNumber !== senderNumber) return !0
 }
 
